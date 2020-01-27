9f6b602480498d62f5c4fc5678a184860cc6306a
don2001l@netscape.net <don2001l@netscape.net>
2020-01-27 15:14:33 -0600

    travis updates ENV etc

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
-docker build -t don2001l/kubernetes-fib-client ./client
-docker build -t don2001l/kubernetes-fib-nginx  ./nginx
-docker build -t don2001l/kubernetes-fib-server ./server
-docker build -t don2001l/kubernetes-fib-worker ./worker
+docker build -t don2001l/kubernetes-fib-client:latest -t don2001l/kubernetes-fib-client:$SHA ./client
+docker build -t don2001l/kubernetes-fib-nginx:latest -t don2001l/kubernetes-fib-nginx:$SHA ./nginx
+docker build -t don2001l/kubernetes-fib-server:latest -t don2001l/kubernetes-fib-server:$SHA ./server
+docker build -t don2001l/kubernetes-fib-worker:latest don2001l/kubernetes-fib-worker:$SHA ./worker
 # push to Docker HUB 
-docker push don2001l/kubernetes-fib-client
-docker push don2001l/kubernetes-fib-nginx
-docker push don2001l/kubernetes-fib-server
-docker push don2001l/kubernetes-fib-worker
+docker push don2001l/kubernetes-fib-client:latest
+docker push don2001l/kubernetes-fib-client:$SHA
+docker push don2001l/kubernetes-fib-nginx:latest
+docker push don2001l/kubernetes-fib-nginx:$SHA
+docker push don2001l/kubernetes-fib-server:latest
+docker push don2001l/kubernetes-fib-server:$SHA
+docker push don2001l/kubernetes-fib-worker:latest
+docker push don2001l/kubernetes-fib-worker:$SHA
